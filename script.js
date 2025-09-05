851ed64815bf129fd025280abcabc747f19b1527
Jesus Moreno <Jesusytty@gmail.com>
2025-09-04 21:02:40 -0500

    15 Mensaje Mostrar alerta al hacer clic en un título de noticia

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Página de noticias tecnológicas cargada ✅");
+
+document.querySelectorAll(".noticia h2").forEach(titulo => {
+    titulo.addEventListener("click", () => {
+        alert("Abriste la noticia: " + titulo.textContent);
+    });
+});
